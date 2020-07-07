48f40651021e586abc0e430f41f8f64dc9507ea3
AiToShiNoHikari <pierre45@hotmail.fr>
2020-07-07 15:21:17 +0200

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,38 @@
-function defaultTask(cb) {
-  // place code for your default task here
-  cb();
+const gulp = require('gulp');
+const sourcemaps = require('gulp-sourcemaps');
+const del = require('del');
+const terser = require('gulp-terser-js')
+const filter = require('gulp-filter');
+
+function cleanScripts() {
+  return del(['dist/**'])
 }
 
-exports.default = defaultTask
+function minifyJs() {
+  return gulp.src('src/**/*.js')
+    .pipe(filter([
+      '**',
+      '!**/__tests__/**',
+      '!**/*.spec.js'
+    ]))
+    .pipe(sourcemaps.init())
+    .pipe(terser({
+      mangle: {
+        toplevel: true
+      }
+    }))
+    .on('error', function(error) {
+      if (error.plugin !== "gulp-terser-js") {
+        console.log(error.message)
+      }
+      this.emit('end')
+    })
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('dist'));
+}
+
+gulp.task('clean-scripts', cleanScripts);
+
+gulp.task('minifyJs', minifyJs);
+
+gulp.task('default', gulp.series(cleanScripts, minifyJs));
