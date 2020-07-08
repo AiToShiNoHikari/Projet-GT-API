ba2ec0f1ce58240a2c519dc669e76cae66cd24ca
AiToShiNoHikari <pierre45@hotmail.fr>
2020-07-08 15:02:05 +0200

    travis sql 6

--- test/travis.sql
+++ test/travis.sql
@@ -1,4 +1,4 @@
-CREATE USER 'ticket'@'localhost' IDENTIFIED WITH mysql_native_password AS 'ticket';
-GRANT USAGE ON *.* TO 'ticket'@'localhost' REQUIRE NONE WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0;
+CREATE USER 'ticket'@'localhost' IDENTIFIED BY 'ticket';
+GRANT USAGE ON *.* TO 'ticket'@'localhost';
 CREATE DATABASE IF NOT EXISTS `ticket`;
 GRANT ALL PRIVILEGES ON `ticket`.* TO 'ticket'@'localhost';
